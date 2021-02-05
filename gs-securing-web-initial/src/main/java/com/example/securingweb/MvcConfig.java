a90f06175a3553c0bcdec6b4340606de6578654a
partha0mishra <partha.mishra@gmail.com>
2021-02-05 15:10:44 -0600

    -a
    Secured web app

--- gs-securing-web-initial/src/main/java/com/example/securingweb/MvcConfig.java
+++ gs-securing-web-initial/src/main/java/com/example/securingweb/MvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 	// View Controllers to expose the views/ Thymeleaf templates
+	// this is an overriding method
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/").setViewName("home");
