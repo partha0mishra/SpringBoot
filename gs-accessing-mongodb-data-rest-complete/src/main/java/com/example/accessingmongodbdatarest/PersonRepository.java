9a3234adecf40b7fc9349cfa95ac6e6fb4c3c52d
partha0mishra <partha.mishra@gmail.com>
2021-02-10 12:03:18 -0600

    final working copy

--- gs-accessing-mongodb-data-rest-complete/src/main/java/com/example/accessingmongodbdatarest/PersonRepository.java
+++ gs-accessing-mongodb-data-rest-complete/src/main/java/com/example/accessingmongodbdatarest/PersonRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// This is really binding the REST part and the MongoDB part
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends MongoRepository<Person, String> {
 
